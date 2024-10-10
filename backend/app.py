6e70e8631828e79d3583bf82556c0e2d261671d3
CrazyChair69 <Christianschwanse@gmx.de>
2024-10-10 17:07:06 +0200

    Change Backend Home Page Rendering + Add Error Message to index.html

--- backend/app.py
+++ backend/app.py
@@ -2,30 +2,38 @@
 Instantiation of Flask application
 """
 
-from flask import Flask
+from flask import Flask, send_from_directory
 import config
 
+
 # Instantiate app
+#
 # Flask app must be created BEFORE you import modules that depend on it!
 # Add them below instatiation of app.
-app = Flask(__name__)
+#
+# All HTML pages are in the frontend folder and
+# **not** in a (Flask) templates folder.
+# Therefore a static folder must be set.
+app = Flask(__name__, static_folder="../frontend")
+
 
 # Load configurations
 app.config.from_object(config)
 
 
 @app.route("/")
-def hello_world():
+def home_page():
     """
-    Return 'Hello World'-paragraph.
+    Render homepage
     """
-    return "<p>Hello, World!</p>"
+    return send_from_directory(app.static_folder, "index.html")
 
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output if errors occur.
     #
-    # ***IMPORTANT***: 'app.debug = True' should be commented out before deploying a production app!!!!
+    # ***IMPORTANT***:
+    # 'app.debug = True' should be commented out before deploying a production app!!!!
     # Good practice for development, but can reveal internal aspects of application
     # --> Security Threat.
     # app.debug = True
