72c6cad3f18fc1739ff2a662951173690bbe97ba
CrazyChair69 <Christianschwanse@gmx.de>
2024-10-10 21:15:03 +0200

    Add property rewrite to Vite proxy + Change Vite Development Server Comment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,19 +6,17 @@ export default defineConfig({
   plugins: [react()],
 
   /*
-      Flask runs the backend and Vite the frontend.
-      To integrate both of them for a full-stack project,
-      you need to connect the two technologies
-      so that Flask takes over the backend and the frontend as well
-      during development.
+      ********* Vite Development Server Settings *********
+      To integrate Flask and Vite for a full-stack project,
+      you need to connect the two technologies during development.
 
       These two parts communicate via HTTP requests (API calls).
       Vite serves as a separate server for the frontend in
       development mode, while Flask delivers both the
-      frontend and the backend in production mode.
+      frontend and the backend in development / production mode.
 
       During development, the Vite server runs independently of the Flask server.
-      To enable Vite to access the Flask backend,
+      To enable Vite to access the Flask server,
       a proxy is set up that forwards API calls
       from http://localhost:5173 (Vite) to http://localhost:5000 (Flask).
 
@@ -30,6 +28,7 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
       },
     },
   },
