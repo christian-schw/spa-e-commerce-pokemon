fa1a57c19bbf691c96eddb840e244cfbe9872398
CrazyChair69 <Christianschwanse@gmx.de>
2024-10-11 13:46:14 +0200

    Rename var 'app' to 'application' for hosting AWS EB

--- backend/app.py
+++ backend/app.py
@@ -17,31 +17,38 @@ from health_check.views import health_check_bp
 # Flask app must be created BEFORE you import modules that depend on it!
 # Add them below instatiation of app.
 #
+# ********* Note about variable app / application *********
+# The Flask app is normally called an 'app'. 
+# The website is hosted with AWS Elastic Beanstalk (EB). 
+# EB searches for 'application' instead of 'app' by default. 
+# This naming convention must be observed!
+#
+# ********* Note about static_folder *********
 # All HTML pages are in the frontend folder and
 # **not** in a (Flask) templates folder.
 # Therefore a static folder must be set.
-app = Flask("Server E-Commerce Pokemon", static_folder=paths.CLIENT_FOLDER)
+application = Flask("Server E-Commerce Pokemon", static_folder=paths.CLIENT_FOLDER)
 
 # Load configurations
-app.config.from_object(config)
+application.config.from_object(config)
 
 # Let the server accept CORS so that the frontend can access the backend, for example.
 # For a start, everything is accepted. Can be adapted later.
-cors = CORS(app, origins="*")
+cors = CORS(application, origins="*")
 
 
 # Register Routes / Views using Blueprints
-app.register_blueprint(home_bp)
-app.register_blueprint(test_api_bp)
-app.register_blueprint(health_check_bp)
+application.register_blueprint(home_bp)
+application.register_blueprint(test_api_bp)
+application.register_blueprint(health_check_bp)
 
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output if errors occur.
     #
     # ***IMPORTANT***:
-    # 'app.debug = True' should be commented out before deploying a production app!!!!
+    # 'application.debug = True' should be commented out before deploying a production app!!!!
     # Good practice for development, but can reveal internal aspects of application
     # --> Security Threat.
-    # app.debug = True
-    app.run()
+    # application.debug = True
+    application.run()
